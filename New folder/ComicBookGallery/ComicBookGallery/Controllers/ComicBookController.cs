0e90dad329e6349a02ae10a92e8e75a4ba93f1c7
Mike Collins <mike@bouncycastlenetwork.co.uk>
2019-08-19 16:20:04 +0100

    Added Comic Book Detail view

--- New folder/ComicBookGallery/ComicBookGallery/Controllers/ComicBookController.cs
+++ New folder/ComicBookGallery/ComicBookGallery/Controllers/ComicBookController.cs
@@ -12,11 +12,7 @@ namespace ComicBookGallery.Controllers
 		public ActionResult Detail()
 		{
 
-			if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
-			{
-				return new RedirectResult("/");
-			}
-			return Content("Hello");
+			return View();
 
 		}
 	}
